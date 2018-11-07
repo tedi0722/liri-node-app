d23b8af34f5c75146b08bad8da315da0292f6d44
tedi <tedi0722@users.noreply.github.com>
2018-11-06 17:05:15 -0800

    final

--- README.md
+++ README.md
@@ -13,3 +13,4 @@
 * do-what-it-says (uses npm inquirer and prompt to let the user choose a command)
 
 #### Video demo of the application: https://drive.google.com/file/d/15bBPXb29qQr744ocfwbdrhCScb0qRKUt/view
+* Note: When passing in a value, quotation marks are only needed when there are more than one word.
--- liri.js
+++ liri.js
@@ -10,30 +10,30 @@ var moment = require('moment');
 var request = require('request');
 var fs = require('fs');
 var action = process.argv[2];
-var value  = process.argv[3]
+var value = process.argv[3]
 
 
 
 switch (action) {
-    
+
     case "concert-this":
-        concertThis (value);
+        concertThis(value);
         break;
 
     case "spotify-this-song":
-        spotifyThisSong (value);
+        spotifyThisSong(value);
         break;
 
     case "movie-this":
-        movieThis (value);
+        movieThis(value);
         break;
-    
+
     case "do-what-it-says":
-        doWhatItSays ();
+        doWhatItSays();
         break;
 };
 
-function concertThis (value) {
+function concertThis(value) {
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
     request(queryUrl, function (error, response, body) {
@@ -45,14 +45,14 @@ function concertThis (value) {
                 console.log("\n");
                 console.log("Venue: " + concertObj[i].venue.name);
                 console.log("Location: " + concertObj[i].venue.country + ", " + concertObj[i].venue.region + ", " + concertObj[i].venue.city);
-                console.log("Date: " + moment(concertObj[i].datetime).format('L') );
+                console.log("Date: " + moment(concertObj[i].datetime).format('L'));
                 console.log("\n");
             };
         }
     });
 }
 
-function spotifyThisSong (value) {
+function spotifyThisSong(value) {
 
     var defaultSong = "The Sign";
 
@@ -64,25 +64,25 @@ function spotifyThisSong (value) {
         type: "track",
         query: value,
         limit: 5
-        }, function (err, data) {
+    }, function (err, data) {
 
-            if (err) {
-                console.log("Error: " + err);
-                return;
-            } else {
-                for (var i = 0; i < 5; i++) {
-                    console.log("\n")
-                    console.log("Artist: " + data.tracks.items[i].artists[0].name);
-                    console.log("Song: " + data.tracks.items[i].name);
-                    console.log("Preview URL: " + data.tracks.items[i].preview_url);
-                    console.log("Album: " + data.tracks.items[i].album.name);
-                    console.log("\n");
-                };
+        if (err) {
+            console.log("Error: " + err);
+            return;
+        } else {
+            for (var i = 0; i < 5; i++) {
+                console.log("\n")
+                console.log("Artist: " + data.tracks.items[i].artists[0].name);
+                console.log("Song: " + data.tracks.items[i].name);
+                console.log("Preview URL: " + data.tracks.items[i].preview_url);
+                console.log("Album: " + data.tracks.items[i].album.name);
+                console.log("\n");
             };
-        });
+        };
+    });
 };
 
-function movieThis (value) {
+function movieThis(value) {
 
     var defaultMovie = "Mr. Nobody";
 
@@ -112,12 +112,11 @@ function movieThis (value) {
     });
 }
 
-function doWhatItSays () {
+function doWhatItSays() {
 
     inquirer
-        .prompt([
-            {
-                type: "list", 
+        .prompt([{
+                type: "list",
                 message: "Choose a command!",
                 choices: ["concert-this", "spotify-this-song", "movie-this"],
                 name: "action"
@@ -127,11 +126,11 @@ function doWhatItSays () {
                 message: "Enter Value",
                 name: "value"
             }
-        ]).then(function(inquirerResponse) {
-            
-                var data = inquirerResponse.action + ", " + JSON.stringify(inquirerResponse.value)
+        ]).then(function (inquirerResponse) {
 
-            fs.writeFile('random.txt', data, function(err) {
+            var data = inquirerResponse.action + ", " + JSON.stringify(inquirerResponse.value)
+
+            fs.writeFile('random.txt', data, function (err) {
 
                 var dataArr = data.split(',');
 
@@ -139,7 +138,7 @@ function doWhatItSays () {
                     console.log("Error: " + err);
                 } else if (dataArr[0] === "concert-this") {
                     dataArr[0] = process.argv[2];
-                    concertThis(dataArr[1]);
+                    concertThis(JSON.parse(dataArr[1]));
                 } else if (dataArr[0] === "spotify-this-song") {
                     dataArr[0] = process.argv[2];
                     spotifyThisSong(dataArr[1]);
@@ -147,9 +146,9 @@ function doWhatItSays () {
                     dataArr[0] = process.argv[2];
                     movieThis(dataArr[1]);
                 }
-                
+
             });
         });
 
-    
+
 }
